11d1a586b41ce2e427c36eabefe44cedaaec7f6b
Tiago Camargo <tiagocamargo@Tiagos-iMac.local>
2023-07-25 00:29:44 -0300

    Adicionando linguagens ao programa

--- hello.py
+++ hello.py
@@ -1,3 +1,37 @@
 #!/usr/bin/python3
 
-print("Hello, World!")
+"""Hello World Multi Linguas
+
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem 
+correspondente.
+
+Como usar:
+
+Tenha a variavel LANG devidamente configurada, ex:
+    export LANG=pt_BR
+
+Execução:
+    python3 hello.py
+    ou
+    ./hello.oy
+"""
+__version__ = "0.0.1"
+__author__= "Tiago Camargo"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour Monde"
+
+print(msg)
